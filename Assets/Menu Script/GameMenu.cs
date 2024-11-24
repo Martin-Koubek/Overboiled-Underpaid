3716c63e8c09a2a14e0993c04f48dbc9135b2818
Martin Koubek <147321704+Martin-Koubek@users.noreply.github.com>
2024-11-24 13:35:36 +0100

    Main menu + pause menu

---
+++ Assets/Menu Script/GameMenu.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject PMenu;
+    public GameObject options;
+    public bool isOptionOpen;
+    public bool isPaused;
+
+    void Start()
+    {
+        isPaused = false;
+        isOptionOpen = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == false)
+            {
+                pauseGame();
+            }
+            else if(isPaused == true && isOptionOpen == false)
+            {
+                resume();
+            }
+            else if (isPaused == true && isOptionOpen == true)
+            {
+                closeSet();
+            }
+            else
+            {
+                resume();
+            }
+        }
+       
+
+    }
+
+    public void openSet()
+    {
+        PMenu.SetActive(false);
+        options.SetActive(true);
+        isOptionOpen = true;
+    }
+    public void closeSet() 
+    {
+        options.SetActive(false);
+        PMenu.SetActive(true);
+        isOptionOpen = false;  
+    }
+
+    public void pauseGame()
+    {
+        PMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void resume()
+    {
+        PMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void BackToMain()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadSceneAsync("menu");
+    }
+
+}
---
+++ Assets/scripts/OptionsScript.cs
@@ -0,0 +1,66 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsScript : MonoBehaviour
+{
+
+    //reference na audioMixer
+    public AudioMixer audio;
+
+    //reference na dropdown
+    public Dropdown resolutionDropdown;
+
+    //nastavování audia
+    public void SetVolume (float volume)
+    {
+        audio.SetFloat("Volume", volume);
+    }
+
+
+
+    Resolution[] res;
+    //pøidávání možností na rozlišení pro uživatele
+    private void Start()
+    {
+        res = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List <string> resOptions = new();
+        int currentResIndex = 0;
+        for (int i = 0; i < res.Length; i++)
+        {
+            string options = res[i].width + "x" + res[i].height;
+            resOptions.Add(options);
+
+            if (res[i].width == Screen.currentResolution.width && res[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(resOptions);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetRes(int ResIndex)
+    {
+        Resolution resolution = res[ResIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    //nastavení kvality
+    public void Quality (int qualitIndex)
+    {
+        QualitySettings.SetQualityLevel(qualitIndex);
+    }
+    
+    //nastavení fullscreenu
+    public void FullScreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+    }
+
+}
+
