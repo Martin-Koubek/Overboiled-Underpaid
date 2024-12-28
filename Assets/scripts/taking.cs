f1c06c4f91af16c2423466d35565361117ac0c01
Martin Koubek <147321704+Martin-Koubek@users.noreply.github.com>
2024-12-28 19:35:26 +0100

    nekompletní item pickup

--- Assets/scripts/taking.cs
+++ Assets/scripts/taking.cs
@@ -1,13 +1,21 @@
-using UnityEditor.Animations;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class taking : MonoBehaviour
 {
+    //reference na prefab ingrediencí
+    public GameObject buns, letus, burgerMeat, cheese;
+ 
     public GameObject hand;
+    public Transform player;
     public GameObject spot;
-    public AnimatorController animator;
-    public bool holding = false;
+    public float IntRange = 5f;
+    public bool holding;
     public float ThrowF = 5f;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,23 +28,30 @@ public class taking : MonoBehaviour
         
         if (Input.GetKey(KeyCode.E))
         {
-            if (!holding)
+            Ray ray = new Ray(player.position, player.forward);
+            if (!holding && Physics.Raycast(ray, out RaycastHit rayInfo, IntRange))
             {
-
+                if(rayInfo.collider.gameObject.CompareTag("bun storage"))
+                {
+                    holding = true;
+                    hand.SetActive(true);
+                    Instantiate(buns, spot.transform.position, Quaternion.identity);
+                }
+                if (rayInfo.collider.gameObject.CompareTag("letus storage"))
+                {
+                    holding = true;
+                    hand.SetActive(true);
+                    Instantiate(letus, spot.transform.position, Quaternion.identity);
+                }
+                if (rayInfo.collider.gameObject.CompareTag("cheese storage"))
+                {
+                    holding = true;
+                    hand.SetActive(true);
+                    Instantiate(cheese, spot.transform.position, Quaternion.identity);
+                }
             }
-            else { }
 
         }
+        //na Q bude throw
     }
-
-    void Throw()
-    {
-
-    }
-
-    void Grab()
-    {
-
-    }
-
 }
