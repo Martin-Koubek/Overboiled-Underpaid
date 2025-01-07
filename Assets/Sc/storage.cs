fbb917dd3d7cddef13c2c0a6bf86467a52110f22
Marti753357 <147321704+Martin-Koubek@users.noreply.github.com>
2025-01-07 09:48:17 +0100

    taking script fix

---
+++ Assets/Sc/storage.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class storage : MonoBehaviour
+{
+    public GameObject FoodRef;
+}
--- Assets/scripts/taking.cs
+++ Assets/scripts/taking.cs
@@ -6,112 +6,41 @@ using UnityEngine.Animations;
 using UnityEngine.UI;
 
 public class taking : MonoBehaviour
-{
-    //reference na prefab ingrediencí
-    public GameObject buns, lettuce, burgerMeat, cheese, steak, carot, tomato, ham, plate, bowl;
-    public LayerMask maskIngred;
-    
+{    
     //reference na držený pøedmìt, mýsto držení, bod poèátku raycastu 
     public GameObject heldItem;
     public Transform RayCastPoint;
     public GameObject spot;
     
     //dosah hráèe
-    public float IntRange = 5f;
-    //drží hráè nìco
+    public float HitRange = 5f;
+    //drží hráè nìco ?
     public bool holding;
     //raycast info
     public RaycastHit hit;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-     
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.E))
         {
             Ray ray = new Ray(RayCastPoint.position, RayCastPoint.forward);
-            if (!holding && Physics.Raycast(ray, out hit, IntRange))
+            if (!holding && Physics.Raycast(ray, out hit, HitRange))
             {
-                Debug.DrawRay(RayCastPoint.position, RayCastPoint.forward * IntRange, Color.red);
+                Debug.DrawRay(RayCastPoint.position, RayCastPoint.forward * HitRange, Color.red);
                 //Rigidbody rb;
-                if (hit.collider.gameObject.CompareTag("bun storage"))
+                if (hit.collider.gameObject.TryGetComponent<storage>(out storage s))
                 {
-                    //hand.SetActive(true);
-                    heldItem = Instantiate(buns, spot.transform.position, Quaternion.identity);
-                    take();
-
-                }
-                else if (hit.collider.gameObject.CompareTag("lettuce storage"))
-                {
-                    //hand.SetActive(true);
-                    heldItem = Instantiate(lettuce, spot.transform.position, Quaternion.identity);
+                    heldItem = Instantiate(s.FoodRef, spot.transform.position, Quaternion.identity);
                     take();
                 }
-                else if (hit.collider.gameObject.CompareTag("tomato storage"))
-                {
-                    //hand.SetActive(true);
-                    heldItem = Instantiate(tomato, spot.transform.position, Quaternion.identity);
-                    take();
-                }
-                else if (hit.collider.gameObject.CompareTag("ham storage"))
-                {
-                    //hand.SetActive(true);
-                    heldItem = Instantiate(ham, spot.transform.position, Quaternion.identity);
-                    take();
-                }
-                else if (hit.collider.gameObject.CompareTag("cheese storage"))
-                {
-                    //hand.SetActive(true);
-                    heldItem = Instantiate(cheese, spot.transform.position, Quaternion.identity);
-                    take();
-                }
-                else if (hit.collider.gameObject.CompareTag("burger meat storage"))
-                {
-                    //hand.SetActive(true);
-                    heldItem = Instantiate(burgerMeat, spot.transform.position, Quaternion.identity);
-                    take();
-                }
-                else if (hit.collider.gameObject.CompareTag("carot storage"))
-                {
-                    //hand.SetActive(true);
-                    heldItem = Instantiate(carot, spot.transform.position, Quaternion.identity);
-                    take();
-
-                }
-                else if (hit.collider.gameObject.CompareTag("steak storage"))
-                {
-                    //hand.SetActive(true);
-                    heldItem = Instantiate(steak, spot.transform.position, Quaternion.identity);
-                    take();
-                }
-                else if (hit.collider.gameObject.layer == maskIngred)
+                if (hit.collider.GetComponent<ingred>())
                 {
                     heldItem = hit.collider.gameObject;
                     take();
                 }
-                else if (hit.collider.gameObject.CompareTag("pan"))
-                {
-                    heldItem = hit.collider.gameObject;
-                    take();
-                }
-                else if (hit.collider.gameObject.CompareTag("plates"))
-                {
-                    heldItem = Instantiate(plate, spot.transform.position, Quaternion.identity);
-                    take();
-                }
-                else if (hit.collider.gameObject.CompareTag("bowls"))
-                {
-                    heldItem = Instantiate(bowl, spot.transform.position, Quaternion.identity);
-                    take();
-                }
-
+                
             }
-            else if (holding && Physics.Raycast(ray, out hit, IntRange))
+            else if (holding && Physics.Raycast(ray, out hit, HitRange))
             {
                 if (hit.collider.gameObject.CompareTag("pan"))
                 {
@@ -130,6 +59,11 @@ public class taking : MonoBehaviour
         
         
     }
+    public void StorageTake(RaycastHit hit)
+    {
+        
+    }
+
     public void drop()
     {
         heldItem.transform.SetParent(null);
